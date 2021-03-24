a60663812ed375bacf911c6888dacc74d878200f
smdeile <smdeile@gmail.com>
2021-03-24 22:56:45 +0200

    Done app

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState, useCallback } from "react";
+import { interval, Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+
+export default function App() {
+  const [sec, setSec] = useState(0);
+  const [status, setStatus] = useState("stop");
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    const unsubscribe$ = new Subject();
+    interval(1000)
+      .pipe(takeUntil(unsubscribe$))
+      .subscribe(() => {
+        if (status === "run") {
+          setSec((val) => val + 1000);
+        }
+      });
+    return () => {
+      unsubscribe$.next();
+      unsubscribe$.complete();
+    };
+  }, [status]);
+
+  const start = useCallback(() => {
+    setStatus("run");
+  }, []);
+
+  const stop = useCallback(() => {
+    setStatus("stop");
+    setSec(0);
+  }, []);
+
+  const reset = useCallback(() => {
+    setSec(0);
+  }, []);
+
+  const isDoubleClick = useCallback(() => {
+    const timeNow = new Date().getTime();
+
+    const isFastDoubleClick = timeNow - timer < 300;
+    setTimer(timeNow);
+
+    if (isFastDoubleClick) {
+      setStatus("wait");
+    }
+  }, [timer]);
+
+  return (
+    <div>
+      <div className="background"></div>
+      <div className="wrapper">
+        <div>
+          <h2 className="timerDisplay">
+            {new Date(sec).toISOString().slice(11, 19)}
+          </h2>
+        </div>
+        <div className="buttonBlock">
+          <button className="start-button" onClick={start}>
+            Start
+          </button>
+          <button className="stop-button" onClick={stop}>
+            Stop
+          </button>
+          <button onClick={reset}>Reset</button>
+          <button onClick={isDoubleClick}>Wait</button>
+        </div>
+      </div>{" "}
+    </div>
+  );
+}
